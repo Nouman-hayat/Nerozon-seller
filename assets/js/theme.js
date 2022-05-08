dfd508d2536f9c02464306a9a3fef54a3aea36f4
Muhammad  Nouman hayat <75477552+Nouman-hayat@users.noreply.github.com>
2022-05-08 17:09:01 +0500

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,4 @@
+// review slider controls
 $(".owl-carousel").owlCarousel({
   items: 2,
   loop: true,
@@ -19,6 +20,57 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+// review slider controls
+// popup display
+
+$(document).ready(function () {
+  // jQuery methods go here...
+
+  // popup display
+  $("#nav-btn").click(function () {
+    $("#overlay-form").css("display", "block");
+    $("#overlay-form-inner").css("display", "block");
+    $("#login-content").css("display", "block");
+    $("#reset-password-content").css("display", "none");
+     $("#change-password-content").css("display", "none");
+  });
+
+  // forget password popup
+  $("#forget-password").click(function () {
+    $("#login-content").css("display", "none");
+    $("#overlay-form").css("display", "block");
+    $("#overlay-form-inner").css("display", "block");
+    $("#reset-password-content").css("display", "block");
+     $("#change-password-content").css("display", "none");
+    
+  });
+  // close popup
+  $(
+    "#overlay-form , #close-icon-wrapper , #pass-form ,#pass-form ,#btn-cancel "
+  ).click(closebtn);
+ 
+  function closebtn() {
+    
+    $("#overlay-form").css("display", "none");
+    $("#overlay-form-inner").css("display", "none");
+    $("#reset-password-content").css("display", "none");
+    $("#pass-form").css("display", "none");
+    $("#pass-form-inner").css("display", "none");
+     $("#login-content").css("display", "none");
+    $("#change-password-content").css("display", "none");
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 

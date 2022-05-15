7dc10d01bbe8007c93a0032eee85b366058ea476
hp <m.nouman.hayat@gmail.com>
2022-05-15 06:56:02 -0700

    added addnewlist page

--- assets/js/seller.js
+++ assets/js/seller.js
@@ -5,9 +5,46 @@ $(document).ready(function () {
     console.log("heloo");
     $("#user-sidebar-menu-wrapper").toggle(4000);
   });
-    $("#close-icon-sidebar").click(function () {
-        $("#user-sidebar-menu-wrapper").toggle(4000);
+  $("#close-icon-sidebar").click(function () {
+    $("#user-sidebar-menu-wrapper").toggle(4000);
+  });
+  selectprod();
+  function selectprod() {
+    $("#listing-select-all-product").click(selectallproduct);
+  }
+  function selectallproduct() {
+    $(".listing-select-product").attr("checked", "checked");
+    $("#listing-select-all-product").click(function () {
+      $(".listing-select-product").removeAttr("checked");
+      selectprod();
     });
+  }
+  // select product page
+  // $("#select-new-product-category-main-wrapper ul li").click(function () {
+  //   $(this).addClass("active");
+  //   $("#select-new-product-category-main-wrapper ul .active #select-new-product-category-sub-wrapper"
+  //   ).css("display", "block");
+  //      $("#select-new-product-category-main-wrapper ul .active  ul li").click(
+  //        function () {
+  //          $("#select-new-product-category-main-wrapper ul .active  ul li").removeClass("active");
+  //          $(this).addClass("active");
+           
+  //          $("#select-new-product-category-main-wrapper ul  .active #select-new-product-category-sub-wrapper ul .active #category-sub-inner-wrapper"
+  //          ).toggle();
+
+          
+            
+           
+  //        }
+
+  //   );
+      
+   
+  // });
+
+  
+ 
+  // select product page
 });
 // user sidebarscript
 // user dashboard
